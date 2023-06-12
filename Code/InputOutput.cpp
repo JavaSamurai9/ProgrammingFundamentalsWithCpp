54cf52fa3566a7d62046d1245dc3cbe298365b56
SamuraiDev <135925936+JavaSamurai9@users.noreply.github.com>
2023-06-12 08:18:58 -0600

    Code day #2

---
+++ Code/InputOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// input
+	int a,b,c;
+	cin >> a >> b >> c;
+
+	// process
+	float average = (a + b + c) / 3.0;
+
+	// output
+	cout << "Average: " << average;
+	return 0;
+}
